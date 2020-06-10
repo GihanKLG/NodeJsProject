5e46c22ee9cc12d1d572f30d5ea493a30852103b
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-06-10 23:33:46 +0530

    try to implement data adding api

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 
 const connection = require('./config/database');
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get('/', (req, res) => {
     connection.query('SELECT * FROM blogpost', function(error,result,fields) {
@@ -15,22 +18,28 @@ app.get('/', (req, res) => {
     })
 });
 
-app.post('/', (req, res) => {
-    console.log(req);
-    const title = req.body.title;
-    const content = req.body.content;
-    const post = {
-        title: title,
-        content: content,
-        created_at: new Date()
-    }
-    connection.query('INSERT INTO `blogpost` SET ?', post, (err) => {
-        if (err) console.log(err);
-        const result = "success";
-        console.log("success");
-        res.json({result}); 
-        // return res.redirect('/');
-    });
+// app.post('/', (req, res) => {
+//     console.log("ok");
+//     console.log(req.body);
+//     // console.log(req);
+//     // const title = req.body.title;
+//     // const content = req.body.content;
+//     // const post = {
+//     //     title: title,
+//     //     content: content,
+//     //     created_at: new Date()
+//     // }
+//     // connection.query('INSERT INTO `blogpost` SET ?', post, (err) => {
+//     //     if (err) console.log(err);
+//     //     const result = "success";
+//     //     console.log("success");
+//     //     res.json({result}); 
+//     //     // return res.redirect('/');
+//     // });
+// });
+
+app.post("/emp", function (req, res) {
+    console.log(req.body.content);
 });
 
 connection.connect((err) => {
